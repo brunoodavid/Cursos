883d63e3222205ae4d12ba5f272b34b6999010c4
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-07 21:56:14 -0300

    aula sobre operadores lógicos

---
+++ DevQuest/modulo-javascript-basico/05-operados-logicos/index.js
@@ -0,0 +1,30 @@
+/*
+
+    == igual 
+    != diferente
+    === identico
+    !== não indentico
+    < menor que 
+    <= menor ou igual
+    > maior que 
+    >= maior ou igual
+
+    tabela verdade do &&
+
+    V e V = V
+    V e F = F
+    F e V = F
+    F e F = F
+
+    OR - || 
+
+    tabela verdade do ||
+
+    V ou V = V
+    V ou F = V
+    F ou V = V
+    F ou F = F
+
+
+    NOT 
+*/
