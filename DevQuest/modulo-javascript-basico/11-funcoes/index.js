b4b3b9ef3caa3af156ede851b29d9ee4df31bd68
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-08 19:56:14 -0300

    aula sobre funcoes

---
+++ DevQuest/modulo-javascript-basico/11-funcoes/index.js
@@ -0,0 +1,22 @@
+/*
+
+    function nomeDaFuncao(){
+        <bloco de execucao>
+    }
+
+    nomeDaFuncao()
+
+*/ 
+                                    //nome padrao
+ function incentivarQuester(nomeQuester = 'Quester'){
+     alert('Muito bem ' + nomeQuester + ', Você chegou ao JS, continue assim!')
+}
+
+incentivarQuester('Bruno')
+
+// function soma(numero1, numero2){
+//     console.log(numero1 + numero2)
+// }
+
+// soma(1,2)
+
