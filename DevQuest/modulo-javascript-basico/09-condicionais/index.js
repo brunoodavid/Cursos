a1b7147cabdc630bcfddbf14b2c5d3e466d2e154
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-08 18:33:41 -0300

    aula sobre condicional if/else

---
+++ DevQuest/modulo-javascript-basico/09-condicionais/index.js
@@ -0,0 +1,19 @@
+let nomeFilme = 'Vingadores'
+
+if(nomeFilme === 'Vingadores'){
+    console.log('É o filme dos Vingadores')
+} else if(nomeFilme === 'Batman vs Superman'){
+    console.log('É o filme do Batman vs Superman')
+} else if(nomeFilme === 'Capitão América'){
+    console.log('É o filme do Capitão América')
+} else {
+    console.log('É outro filme')
+}
+
+// Operador ternario
+//  condicao ? console.log('true') : console.log('false')
+
+
+true ? console.log('Condição verdadeira') : console.log('condição falsa')
+
+nomeFilme === 'Vingadores' ? console.log('É o filme dos Vingadores') : console.log('É outro filme')
