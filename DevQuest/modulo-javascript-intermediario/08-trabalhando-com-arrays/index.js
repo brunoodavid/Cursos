232dab17c4e5c5c4c0af70e815e9c7697520db5a
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-09 19:52:22 -0300

    aula sobre como trabalhar com arrays

---
+++ DevQuest/modulo-javascript-intermediario/08-trabalhando-com-arrays/index.js
@@ -0,0 +1,25 @@
+let frutas = ['banana', 'maça', 'laranja', 'pêra']
+
+// for(let i = 0; i < frutas.length; i++){
+//     console.log(frutas[i])
+// }
+
+// frutas.forEach(function(item, indice, array){
+//     console.log(frutas[indice])
+// })
+
+
+// adiciona elementos no final do array
+frutas.push('manga')
+
+// remove o elemento final do array
+frutas.pop()
+
+// remove o primeiro elemento do array
+frutas.shift()
+
+//adiciona um elemento no inicio do array
+frutas.unshift('Morango')
+
+// busca qual posição ta o elemnto a ser pesquisado
+frutas.indexOf('laranja')
