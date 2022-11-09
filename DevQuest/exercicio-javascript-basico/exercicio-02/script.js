ad3857c33cfd6c3e635ba230419d80dca2aee1f0
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-08 21:21:25 -0300

    exercicios javascript básico resolvidos

---
+++ DevQuest/exercicio-javascript-basico/exercicio-02/script.js
@@ -0,0 +1,8 @@
+let boletoPago = true
+
+if(boletoPago){
+    console.log("O boleto está pago")
+} else {
+    console.log("O boleto não está pago")
+}
+
---
+++ DevQuest/exercicio-javascript-basico/exercicio-05/script.js
@@ -0,0 +1,4 @@
+for(let i = 0; i <= 10; i++){
+    let y = i * 2
+    console.log("O dobro de " + i + " é " + y);
+}
