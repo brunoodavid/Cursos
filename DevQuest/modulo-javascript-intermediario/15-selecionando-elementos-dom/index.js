3e472412a043129c47a6acdf3341450d2f78c6a5
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-10 20:54:59 -0300

    aula sobre como usar o document para selecionar elementos

---
+++ DevQuest/modulo-javascript-intermediario/15-selecionando-elementos-dom/index.js
@@ -0,0 +1,19 @@
+// console.log(document)
+
+
+// let p = document.getElementById('paragrafo1')
+
+
+// console.log(p)
+
+// let inputPorClasse = document.getElementsByClassName('tag-de-input');
+
+// console.log(inputPorClasse[0])
+
+// let inputPorTag = document.getElementsByTagName('input')
+
+// console.log(inputPorTag)
+
+let inputPorName = document.querySelectorAll("input[name='email']")
+
+console.log(inputPorName)
