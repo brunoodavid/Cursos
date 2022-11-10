5af063fb8dbe1bcf84fdabc5b11f7aec70549d82
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-09 21:26:37 -0300

    aula sobre splice e slice

---
+++ DevQuest/modulo-javascript-intermediario/09-splice-slice/index.js
@@ -0,0 +1,18 @@
+let frutas = ['Banana', 'Maça', 'Laranja', 'Uva']
+
+// let frutasExtraidas = frutas.slice(1,3)
+
+// console.log(frutas)
+
+// console.log(frutasExtraidas)
+
+
+// let frutasExtraidas = frutas.splice(1, 2)
+
+// console.log(frutasExtraidas)
+
+// console.log(frutas)
+
+let removeEAdciona = frutas.splice(1, 2, 'Goiaba', 'Abacaxi', 'Pêra')
+
+console.log(frutas)
