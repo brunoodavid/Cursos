9e33f7d2778406fa77b8e95c9e82158799409223
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-13 13:03:37 -0300

    encontrando e interpretando erros no JS

---
+++ DevQuest/modulo-javascript-intermediario/19-resolvendo-erros-js/index.js
@@ -0,0 +1,12 @@
+// let valorBoleto = 100;
+
+// console.log(valorBolet)
+
+let inputTelefone = document.getElementById('telefone');
+
+inputTelefone.disabled = true;
+
+function habilitarTelefone(){
+    let inputTelefone = document.getElementById('telefone');
+    inputTelefone.disabled = false;
+}
