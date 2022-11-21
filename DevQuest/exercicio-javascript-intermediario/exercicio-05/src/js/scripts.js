c0ba864a3513e290b2b709449ceb4ded269ec44c
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-21 20:11:10 -0300

    exercicio 05 em andamento

---
+++ DevQuest/exercicio-javascript-intermediario/exercicio-05/src/js/scripts.js
@@ -0,0 +1,11 @@
+let imagens = document.querySelectorAll(".slider")
+let setaAvancar = document.getElementById("seta-avancar")
+let setaVoltar = document.getElementById("seta-voltar")
+
+let imagemAtual = 0
+
+setaAvancar.addEventListener("click", function(){
+
+    
+
+})
