7b6e2bf6339e969083f19ca6adb4666c11c29cfd
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-22 18:42:26 -0300

    exercicio 05 finalizado

--- DevQuest/exercicio-javascript-intermediario/exercicio-05/src/js/scripts.js
+++ DevQuest/exercicio-javascript-intermediario/exercicio-05/src/js/scripts.js
@@ -6,6 +6,49 @@ let imagemAtual = 0
 
 setaAvancar.addEventListener("click", function(){
 
-    
+    if(imagemAtual === imagens.length - 1){
+        return
+    }
 
+    imagemAtual++
+
+    esconderImagemAberta();
+    mostrarImagem();
+    mostrarOuEsconderSetas();
 })
+    
+function esconderImagemAberta(){
+    const imagemAberta = document.querySelector(".mostrar")
+    imagemAberta.classList.remove("mostrar")
+}
+
+function mostrarImagem(){
+    imagens[imagemAtual].classList.add("mostrar")
+}
+
+setaVoltar.addEventListener("click", function() {
+    if(imagemAtual === 0){
+        return
+    }
+
+    imagemAtual--;
+    esconderImagemAberta();
+    mostrarImagem();
+    mostrarOuEsconderSetas();
+})
+
+function mostrarOuEsconderSetas(){
+    const naoEhAPrimeiraImagem = imagemAtual !== 0;
+    if(naoEhAPrimeiraImagem){
+        setaVoltar.classList.remove("opacidade");
+    } else{
+        setaVoltar.classList.add("opacidade");
+    }
+
+    const chegouNaUltimaImagem = imagemAtual !== 0 && imagemAtual === imagens.length -1;
+    if(chegouNaUltimaImagem){
+        setaAvancar.classList.add("opacidade");
+    } else{
+        setaAvancar.classList.remove("opacidade");
+    }
+}
