701e43b1404828c675e5609ab4adbfebbf148e5b
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-22 21:25:49 -0300

    exercicio 06 em andamento

---
+++ DevQuest/exercicio-javascript-intermediario/exercicio-06/src/js/script.js
@@ -0,0 +1,9 @@
+const setaFechada = document.querySelectorAll(".seta-fechada")
+
+console.log(setaFechada)
+
+setaFechada.forEach(function(item){
+    item.addEventListener("click", function(){
+        
+    })
+})
