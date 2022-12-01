555618b14251d303afb7446d5b3e8ec8d8772489
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-12-01 18:56:52 -0300

    aula sobre desestruturacao de objetos e array

---
+++ DevQuest/modulo-javascript-avancado/05-desestruturacao-objetos-arrays/script.js
@@ -0,0 +1,23 @@
+let pessoa = {nome: 'Bruno', sobrenomePessoa: 'Hoffmann', idade: 26}
+
+// let nome = pessoa.nome
+// let sobrenome = pessoa.sobrenome
+// let idade = pessoa.idade
+
+// console.log(nome)
+// console.log(sobrenome)
+// console.log(idade)
+
+// let {nome, sobrenomePessoa:sobrenome, idade} = pessoa
+
+// console.log(nome)
+// console.log(sobrenome)
+// console.log(idade)
+
+let numeros = [1,2,3]
+
+let [um,dois,tres] = numeros
+
+console.log(um)
+console.log(dois)
+console.log(tres)
