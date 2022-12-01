5d2353c3967705c8b9f5c3da6866d2c2bd1ce8a6
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-12-01 19:21:05 -0300

    aula sobre spread

---
+++ DevQuest/modulo-javascript-avancado/06-operador-spread/script.js
@@ -0,0 +1,22 @@
+// const pessoas = ['Bruno', 'Ricardo', 'Roberto']
+// console.log(pessoas)
+// console.log(...pessoas)
+
+// CONCATENAR DOIS ARRAYS 
+
+// let pessoas1 = ['Bruno', 'Ricardo', 'Roberto']
+// let pessoas2 = ['Pedro', 'João', 'Paulo']
+
+// pessoas1 = pessoas1.concat(pessoas2)
+// console.log(pessoas1)
+
+// pessoas1 = [ ...pessoas1, ...pessoas2]
+// console.log(pessoas1)
+
+// CLONAR OBJETO
+
+const pessoa1 = {nome: 'Bruno', idade: 26}
+
+const objetoClonado = { ...pessoa1}
+console.log(pessoa1)
+console.log(objetoClonado)
