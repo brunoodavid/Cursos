30ea7d3cc2a44b70d9f71f8d4e2b9ed9654730c1
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-11-30 21:50:31 -0300

    aula sobre interpolação de string

---
+++ DevQuest/modulo-javascript-avancado/04-interpolacao-de-string/script.js
@@ -0,0 +1,11 @@
+// let imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+//     return "Primeiro Nome:" + primeiroNome + " Último Nome: " + ultimoNome + "  Idade:" + idade;
+// }
+
+// console.log(imprimirInformacoesPessoa("Bruno", "Hoffmann", 26))
+
+let imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+    return `Primeiro nome: ${primeiroNome}, Último nome: ${ultimoNome}, Idade: ${idade}`
+}
+
+console.log(imprimirInformacoesPessoa("Bruno", "Hoffmann", 26))
