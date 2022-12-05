7b4989fb0c1b3c6737fb7b579ce53c3952a51652
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2022-12-05 19:46:20 -0300

    aula sobre tratamento de erros

---
+++ DevQuest/modulo-javascript-avancado/13-tratamento-de-erros/index.js
@@ -0,0 +1,50 @@
+let chaleiraEstaNoFogao = true;
+let fogaoEstaLigado = true
+
+let ferverAgua = function(chaleiraEstaNoFogao,fogaoEstaLigado){
+    return new Promise((resolve,reject)=>{
+        if(chaleiraEstaNoFogao && fogaoEstaLigado){
+            console.log('Passo 1 finalizado: Água foi fervida')
+            resolve()
+        } else {
+            console.log('É necessário ligar o fogão e colocar a chaleira no fogão para ferver a água')
+            reject()
+        }
+    })
+}
+
+let passarOCafe = (aguaFervida) => {
+    return new Promise((resolve) => {
+        console.log('Passo 2 finalizado: Café foi passado')
+        resolve(true)
+    })
+}
+
+let tomarCafe = (cafePassado) => {
+    return new Promise((resolve)=>{
+        console.log('Passo 3 finalizado: Terminei de tomar o café')
+        resolve(true)
+    })
+}
+
+let lavarXicara = (cafeTomado) =>{
+    return new Promise((resolve)=>{
+        console.log('Passo 4 finalizado: Terminei de lavar a xícara')
+        resolve(true)
+    })
+}
+
+async function iniciarProcessoDeFazerCade(){
+    try{
+        const aguaFervida = await ferverAgua(chaleiraEstaNoFogao, fogaoEstaLigado)
+        const cafePassado = await passarOCafe(aguaFervida)
+        const cafeTomado = await tomarCafe(cafePassado)
+        const xicaraLavada = await lavarXicara(cafeTomado)
+    }catch(error){
+        console.log(error)
+    }finally{
+        console.log('Finalizado o ritual de tomar o café, bora trabalhar')
+    }
+}
+
+iniciarProcessoDeFazerCade()
