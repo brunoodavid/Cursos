29d9ece48f668abb21387a8a942c66d946cc0f15
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2023-03-06 18:36:39 -0300

    listas no react e a prop key

--- DevQuest/modulo-base-react/05-create-react-app/primeiro-app/src/components/cards/cards.js
+++ DevQuest/modulo-base-react/05-create-react-app/primeiro-app/src/components/cards/cards.js
@@ -1,25 +1,21 @@
 import Card from '../card/card'
 
+const cardsTitle = ['Titulo card 1', 'Titulo card 2', 'Titulo card 3']
+
 const Cards = () => {
     return (
         <div>
             <h2>Meus cards</h2>
             
             <div>
-                <Card>
-                    <h3>Titulo card 1</h3>
-                    <p>Esse é um texto do card 1</p>
-                </Card>
-
-                <Card>
-                    <h3>Titulo card 2</h3>
-                    <p>Esse é um texto do card 1</p>
-                </Card>
-
-                <Card>
-                    <h3>Titulo card 3</h3>
-                    <p>Esse é um texto do card 1</p>
-                </Card>
+                {cardsTitle.map((cardTitle, index) => {
+                    return (
+                        <Card key={index}>
+                            <h3>{cardTitle}</h3>
+                            <p>Esse é um texto do card 1</p>
+                        </Card>
+                    )
+                })}
             </div>
         </div>
     )
