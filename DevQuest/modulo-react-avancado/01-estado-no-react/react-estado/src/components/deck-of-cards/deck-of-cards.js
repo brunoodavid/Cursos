9d5c2aae170cc77ee221c0c73e81c1afb975b4cd
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2023-03-08 16:17:01 -0300

    aula sobre renderização condicional

--- DevQuest/modulo-react-avancado/01-estado-no-react/react-estado/src/components/deck-of-cards/deck-of-cards.js
+++ DevQuest/modulo-react-avancado/01-estado-no-react/react-estado/src/components/deck-of-cards/deck-of-cards.js
@@ -13,6 +13,22 @@ async function getCards(deckId) {
     return await response.json()
 }
 
+const CardsList = (props) => {
+    return (
+        <ul>
+            {
+                props.cards.map((card, index) => {
+                    return (
+                        <li key={index}>
+                            <img src={card.image} alt={card.value} />
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
 const DeckOfCards = () => {
     const [deck, setDeck] = useState({
         cards: []
@@ -33,17 +49,7 @@ const DeckOfCards = () => {
 
     return (
         <section>
-            <ul>
-                {
-                    deck.cards.map((card, index) => {
-                        return(
-                            <li key={index}>
-                                <img src={card.image} alt={card.value} />
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+            {deck.cards.length > 0 ? <CardsList cards={deck.cards}/> : "Nenhuma carta encontrada!"}
         </section>
     )
 }
