49a0b15150b3ad002f3e311eacb34b21f8ee457f
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2023-03-08 16:12:24 -0300

    aula sobre hook de efeito - useEffect

--- DevQuest/modulo-react-avancado/01-estado-no-react/react-estado/src/components/deck-of-cards/deck-of-cards.js
+++ DevQuest/modulo-react-avancado/01-estado-no-react/react-estado/src/components/deck-of-cards/deck-of-cards.js
@@ -1,28 +1,42 @@
-import {Component} from 'react'
-import {useState} from 'react'
+import { Component } from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
-async function createDeck(){
+async function createDeck() {
     const response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
     const deck = await response.json()
-    return deck.deck_id 
+    return deck.deck_id
 }
 
-async function getCards(deckId){
+async function getCards(deckId) {
     const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`)
     return await response.json()
 }
 
 const DeckOfCards = () => {
-    const [deck, setDeck]  = useState({
+    const [deck, setDeck] = useState({
         cards: []
     })
 
-    return(
+    useEffect(() => {
+        const fetchData = async () => {
+            const deckId = await createDeck()
+            const data = await getCards(deckId)
+
+            setDeck({
+                cards: data.cards
+            })
+        }
+
+        fetchData()
+    }, [])
+
+    return (
         <section>
             <ul>
                 {
                     deck.cards.map((card, index) => {
-                        (
+                        return(
                             <li key={index}>
                                 <img src={card.image} alt={card.value} />
                             </li>
